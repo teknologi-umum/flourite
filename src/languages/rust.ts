febfb8ef203f6a1d1d9662240a5a0cd5681e724c
Reinaldy Rafli <aldy505@tutanota.com>
2021-08-16 14:23:03 +0700

    feat: added more points

--- src/languages/rust.ts
+++ src/languages/rust.ts
@@ -2,20 +2,22 @@ import type { LanguagePattern } from '../types';
 
 export const Rust: LanguagePattern[] = [
   { pattern: /fn\smain()/, points: 2 },
-  { pattern: /fn\s[A-Za-z0-9<>,]+\(.*\)\s->\s\w+(\s\{|)/, points: 1 },
+  { pattern: /(pub\s)?fn\s[A-Za-z0-9<>,]+\(.*\)\s->\s\w+(\s\{|)/, points: 1 },
   { pattern: /let\smut\s\w+(\s=|)/, points: 2 },
-  { pattern: /(println|print)!\(.*\)/, points: 2 },
+  { pattern: /(.*)!\(.*\)/, points: 1 },
   { pattern: /use\s\w+::.*/, points: 2 },
   { pattern: /\{:\?\}/, points: 1 },
   { pattern: /loop \{/, points: 1 },
-  { pattern: /impl/, points: 1 },
+  // Rust keywords
+  { pattern: /(impl|crate|extern|macro|box)/, points: 2 },
   { pattern: /match\s\w+\s\{/, points: 1 },
-  { pattern: /macro_rules!/, points: 1 },
   { pattern: /\w+\.len\(\)/, points: 1 },
   // Data types
   { pattern: /(&str|(i|u)(8|16|32|64|128|size))/, points: 1 },
   // Vector
   { pattern: /(Vec|Vec::new)|vec!/, points: 2 },
+  // Traits
+  { pattern: /(Ok|Err|Box|ToOwned|Clone)/, points: 1 },
   // Panic!!
   { pattern: /panic!\(.*\)/, points: 2 },
 ];
