bca7431880e2018aea131b7cf7f2f588533bb279
ryuhojin <interarea@naver.com>
2022-11-04 15:49:01 +0900

    fix Markdown regex

--- src/languages/markdown.ts
+++ src/languages/markdown.ts
@@ -3,8 +3,8 @@ import type { LanguagePattern } from "../types";
 export const Markdown: LanguagePattern[] = [
   // headings
   { pattern: /^(#){2,6}\s.+/, type: "keyword" },
-  // headings alternate syntax
-  { pattern: /^(?!!)(=|-){2,}(?<!>)$/, type: "meta.module" },
+  // headings alternate syntax, this code not working in Safari, Firfox
+  // { pattern: /^(?!!)(=|-){2,}(?<!>)$/, type: "meta.module" },
   // images
   { pattern: /(!)?\[.+\]\(.+\)/, type: "keyword" },
   // links 2
